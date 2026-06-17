4fba870ce3d1bbc37f1996129b4af9944570d52f
Jain.Sweta <jain.sweta1020@gmail.com>
2026-06-17 01:40:44 -0400

    api crud

--- src/test/java/API_CRUD/testAPI_CRUD.java
+++ src/test/java/API_CRUD/testAPI_CRUD.java
@@ -1,13 +1,111 @@
 package API_CRUD;
 
+import org.testng.Assert;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.http.ContentType;
+
+
 
 public class testAPI_CRUD {
 	
 	@Test
-	public void testGet() {
-		RestAssured.get();		
+	public void testGetResponse() {	
+		Response response = get("http://localhost:3000/users/1");
+		System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+	}
+		
+	@Test
+	public void testGet() {	
+		baseURI  = "http://localhost:3000";
+		
+		given().
+			param("firstName", "Peter").
+			get("/users").
+		then().
+			statusCode(200).
+			body("id", hasItems("2")).
+			log().all();
+	}
+	
+	@Test
+	public void testPost() {
+		JSONObject request = new JSONObject();
+		
+		request.put("firstName", "Barack");
+		request.put("lastName", "Obama");
+		request.put("subjectId", 2);
+		
+		baseURI  = "http://localhost:3000/";
+		given().header("Content-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("/users/").
+		then().
+			statusCode(201).
+			log().all();
+	}
+	
+	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		
+		request.put("lastName", "Hussein");
+		
+		baseURI  = "http://localhost:3000/";
+		given().header("Content-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("/users/5").
+		then().
+			statusCode(200).
+			log().all();
+	}
+	
+	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		
+		request.put("firstName", "Michelle");
+		request.put("lastName", "Obama");
+		request.put("subjectId", 1);
+		
+		baseURI  = "http://localhost:3000/";
+		given().header("Content-Type", "application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("/users/5").
+		then().
+			statusCode(200).
+			log().all();
+	}
+	
+	@Test
+	public void testDelete() {
+		baseURI  = "http://localhost:3000/";
+		
+		when().
+			delete("/users/5").
+		then().
+			statusCode(200).
+			log().all();
 	}
 }
